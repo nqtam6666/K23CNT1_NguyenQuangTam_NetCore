5756a9b0a514347bd2c1ea3af63f087bfbd0078c
Nguyễn Quang Tâm <nguyenquangtam179@gmail.com>
2025-04-21 10:51:35 +0700

    day01

---
+++ Lesson1_mvc/Lesson1_mvc/Controllers/NqtDemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1_mvc.Controllers
+{
+    public class NqtDemoController : Controller
+    {
+        public IActionResult NqtIndex2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson1_mvc/Lesson1_mvc/Controllers/NqtHomeController.cs
@@ -0,0 +1,32 @@
+using Lesson1_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson1_mvc.Controllers
+{
+    public class NqtHomeController : Controller
+    {
+        private readonly ILogger<NqtHomeController> _logger;
+
+        public NqtHomeController(ILogger<NqtHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult NqtIndex()
+        {
+            return View();
+        }
+
+        public IActionResult NqtPrivacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

70a5ad3d334e40af5873f6708d146fec10856170
NoPainNoGain <NoPainNoGain@DESKTOP-5VDN8L9>
2025-05-18 09:44:29 +0700

    lab03

---
+++ Lab03/Lab03/Controllers/NqtHomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3.Controllers
+{
+    public class NqtHomeController : Controller
+    {
+        private readonly ILogger<NqtHomeController> _logger;
+
+        public NqtHomeController(ILogger<NqtHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult NqtIndex()
+        {
+            var products = new List<NqtProduct>
+            {
+                new NqtProduct { Id = 1, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" },
+                new NqtProduct { Id = 2, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" },
+                new NqtProduct { Id = 3, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" }
+            };
+            return View(products);
+        }
+
+        public IActionResult NqtAbout()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult GetHotProducts()
+        {
+            // Giả lập dữ liệu (thay bằng truy vấn từ database nếu cần)
+            var products = new List<NqtProduct>
+        {
+            new NqtProduct { Id = 1, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" },
+            new NqtProduct { Id = 2, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" },
+            new NqtProduct { Id = 3, Name = "Nồi cơm điện cao tần Nagakawa NAG0102", Image = "/product1.jpg" }
+        };
+
+            return PartialView("_NqtHotProduct", products);
+        }
+    }
+}
---
+++ Lab03/Lab03/ViewComponents/NqtHotProductViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class NqtHotProductViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var hotProducts = new List<NqtProduct>
+        {
+            new NqtProduct { Name = "Nồi cơm điện tử Nagakawa NAG0102", Image = "hotproduct1.jpg" },
+            new NqtProduct { Name = "Nồi cơm điện tử Nagakawa NAG0102", Image = "hotproduct2.jpg" },
+            new NqtProduct { Name = "Nồi cơm điện tử Nagakawa NAG0102", Image = "hotproduct3.jpg" }
+        };
+        return View(hotProducts);
+    }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public string Image { get; set; }
+}

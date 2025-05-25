5f88b047eed07d281cc5ddbdfc7a039b9178b6e7
NoPainNoGain <NoPainNoGain@DESKTOP-5VDN8L9>
2025-05-25 20:55:07 +0700

    Lesson 05 Models

---
+++ Lesson05 Models/Lesson05 Models/Controllers/NqtHomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Lesson05_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson05_Models.Controllers
+{
+    public class NqtHomeController : Controller
+    {
+        private readonly ILogger<NqtHomeController> _logger;
+
+        public NqtHomeController(ILogger<NqtHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult NqtIndex()
+        {
+            return View();
+        }
+        public IActionResult NqtAbout()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lesson05 Models/Lesson05 Models/Controllers/NqtMemberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lesson05_Models.Models.DataModels;
+
+namespace Lesson05_Models.Controllers
+{
+    public class NqtMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NqtGetMember()
+        {
+            var NqtMember = new NqtMember();
+            NqtMember.NqtMemberId = Guid.NewGuid().ToString();
+            NqtMember.NqtUserName = "nqtam6666";
+            NqtMember.NqtFullName = "Nguyên Quang Tâm";
+            NqtMember.NqtPassword = "11111";
+            NqtMember.NqtEmail = "nguyenquangtam179@gmail.com";
+
+            ViewBag.NqtMember = NqtMember;
+            return View();
+        }
+    }
+}
